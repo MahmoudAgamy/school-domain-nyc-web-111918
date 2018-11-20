c906844f4839ad7f68a4f8fe3fb652be315c3edd
Mahmoud Agamy <agamyv@gmail.com>
2018-11-20 22:31:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,5 +10,11 @@ class School
   
   def add_student(student_name, grade)
     if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+    
   end
 end
